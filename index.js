bbd5bcb7fcdae4e60dcc7761d4b9b05c3901596a
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2020-10-24 08:49:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var port = process.env.PORT || 3000;
 const JSONdb = require('simple-json-db');
 cont db = new JSONdb(__dirname + '/db.json');
 
+const delete_check_key = process.env.DELKEY;
+
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,6 +20,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const push_value_error_one = 'Push Value FAILED due to the condition being set to EDIT and the key having no value.';
 const push_value_error_two = 'Push Value FAILED due to no condition being set. Set your condition as NEW or EDIT.';
 const push_value_error_three = 'Push Value FAILED due to the condition being set to NEW despite the key having a value.';
+const push_value_error_four = 'Push Value FAILED due to no condition being set. Set your condition to EDIT.';
+
+const delete_value_error_one = 'Delete Value FAILED due to the deletion key being incorrect.';
 
 app.post("/push", function (req, res) {
   const push_key = req.body.push_key;
@@ -43,6 +48,38 @@ app.post("/push", function (req, res) {
     if (push_condition === 'new' || push_condition === 'NEW') {
       res.send('Push Value FAILED: ' + push_value_error_three);
     }
+    else if (push_condition === 'edit' || push_condition === 'EDIT') {
+      const push_appended_key_value = push_key_value_check + push_value;
+      
+      db.set(push_key, push_appended_key_value);
+      
+      res.send('Push Value SET');
+    }
+    else {
+      res.send('Push Value FAILED: ' + push_value_error_four);
+    }
+  }
+});
+
+app.post("/get", function (req, res) {
+  const push_key = req.body.push_key;
+  
+  const push_key_value = db.get(push_key);
+  
+  res.send(push_key_value);
+});
+
+app.post("/delete", function (req, res) {
+  const push_key = req.body.push_key;
+  const deletion_key = req.body.deletion_key;
+  
+  if (deletion_key === delete_check_key) {
+    db.delete(push_key);
+    
+    res.send('Delete Value SET');
+  }
+  else {
+    res.send('Delete Value FAILED: ' + delete_value_error_one);
   }
 });
 
